3b289a367826222f5b0aa23d2a5ab896591555e0
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-09-01 00:03:11 +0800

    0.1.4_facebook

--- core/server/routes.js
+++ core/server/routes.js
@@ -7,10 +7,16 @@
 var errors = require('./components/errors');
 
 module.exports = function(app) {
+  //app.all('*', function(req, res, next) {
+  //  res.header('Access-Control-Allow-Origin', '*');
+  //  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  //  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
+  //  next();
+  //});
 
   // Insert routes below
   //app.use('/api/things', require('./api/thing'));
-
+  //
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|blib|assets)/*')
    .get(errors[404]);
@@ -20,4 +26,5 @@ module.exports = function(app) {
     .get(function(req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
+
 };
