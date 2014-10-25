fef13b77c45f3c30c1525c2290687397eaa1ccc6
Speed.Of.Light <speed.of.lightt@gmail.com>
2014-10-26 00:48:21 +0100

    new teacher group

--- config/deploy/lab.rb
+++ config/deploy/lab.rb
@@ -1,17 +1,17 @@
-set :stage, :production
+set :stage, :lab
 set :branch, "master"
 
 set :deploy_to, "/home/#{fetch(:deployer)}/app/#{fetch(:application)}"
 
 set :enable_ssl, false
 
-role :app, %w{nsn@140.122.183.107}
-role :web, %w{nsn@140.122.183.107}
-role :db,  %w{nsn@140.122.183.107}
+role :app, %w{nsn@140.122.184.58}
+role :web, %w{nsn@140.122.184.58}
+role :db, %w{nsn@140.122.184.58}
 
 set :ssh_options, {
   keys: %w(~/.ssh/id_rsa),
   forward_agent: true,
-  port: 18325,
+  port: 18458,
   auth_methods: %w(publickey)
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
+## installation
+
+- setup nginx
+- setup upstart
+  - sudo vim /etc/init/bras.conf
+  - sudo vim /etc/dbus-1/system.d/Upstart.conf
+- scp -r /shared
+- cap [stage] deploy
+
 ### Possibly errors
 
 - `bower install`: resulted by dependency conflict
